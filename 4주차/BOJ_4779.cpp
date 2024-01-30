6ec3258f8b5383ebd3a652fa6bfdcba4c5f1f135
박성진 <106302098+macboy5@users.noreply.github.com>
2024-01-30 13:45:54 +0900

    Create BOJ_4779.cpp

---
+++ 4주차/BOJ_4779.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>	
+using namespace std;
+
+void cannto(int N) {
+	int len = pow(3, N);
+	if (len ==1) {
+		cout << "-";
+		return;
+	}
+	else {
+		cannto(N - 1);
+
+		for (int i = 0; i < len / 3; i++)
+			cout << " ";
+
+		cannto(N - 1);
+	}
+
+}
+
+int main(void) {
+	int N;
+	
+	while (cin >> N) {
+
+		cannto( N);
+		cout << "\n";
+	}
+
+		return 0;
+}
