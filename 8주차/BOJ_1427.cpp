c16616b6f19c2e4a0b2248a44675250ed145e0d7
박성진 <106302098+macboy5@users.noreply.github.com>
2024-02-22 01:18:54 +0900

    Create 소트인사이드.cpp

---
+++ 8주차/BOJ_1427.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+int main() {
+
+
+    int arr[10]={0};
+
+    string num;
+    cin >> num;
+    for (int i = 0; i < num.size(); i++) {
+        int tmp = num[i] - '0';
+        arr[tmp] += 1;
+    }
+
+    for (int i = 9; i >= 0; i--) {
+        int temp= arr[i];
+        for (int j = temp; j > 0; j--) {
+            cout << i;
+        }
+    }
+
+    return 0;
+}
