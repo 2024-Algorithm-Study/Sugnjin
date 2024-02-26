eb871e7c3f4b839fdac60d62c46988a9fa0922d8
박성진 <106302098+macboy5@users.noreply.github.com>
2024-02-26 15:40:38 +0900

    Create BOJ_1181_단어정렬.cpp

---
+++ 8주차/BOJ_1181.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+void sort(vector<string> &word) {
+    //길이 짧은 순, 길이 같을 때는 사전 순 정렬.
+
+
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<string> word;
+
+    for (int i = 0; i < N; i++) {
+        string tmp;
+        cin >> tmp;
+        word.push_back(tmp);
+    }
+
+    sort(word);
+
+
+    return 0;
+}
